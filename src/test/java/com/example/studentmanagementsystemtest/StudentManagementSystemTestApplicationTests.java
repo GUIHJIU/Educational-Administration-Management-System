9cad6ae413bc044cc1fe02156f3397ba09f37dd4
3058509009 <3058509009@qq.com>
2024-12-17 15:57:54 +0800

    Initial commit

---
+++ src/test/java/com/example/studentmanagementsystemtest/StudentManagementSystemTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentmanagementsystemtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystemTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
